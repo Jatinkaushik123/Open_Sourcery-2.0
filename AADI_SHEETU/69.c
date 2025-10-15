9bb4c6a9f0720ce2f5c166e8a990245d0106cfbb
Jatin Kaushik <kaushikjatin2608@gmail.com>
2025-10-15 15:39:55 +0530

    first commit

---
+++ AADI_SHEETU/69.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int buggy_factorial(int num) {
+    if (num < 0) return -1; // Error check! not defined for -ve no, (returns -1)
+     int result = num; 
+    if (num == 0) {
+    } // Main bug I think so!
+
+    for (int i = num - 1; i > 1; i--) {
+        result = result * i;
+    }
+    return result;
+}
+
+int main() {
+    int number = 5;
+    int fact_result;
+
+    fact_result = buggy_factorial(number);
+
+    if (fact_result == -1) {
+        printf("Factorial is not defined for negative numbers.\n");
+    } else {
+        printf("The factorial of %d is %d.\n", number, fact_result);
+    }
+    return 0;
+}
